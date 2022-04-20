c1345ee0504b81b0f1672dd316e716645bf70dfe
DERRICKDRANGO <>
2022-04-20 16:12:44 +0000

    part 3 done

--- CropInfo.cpp
+++ CropInfo.cpp
@@ -1,18 +1,30 @@
 #include "CropInfo.h"
 #include "ReadUtils.h"
 #include <iostream>
+#include <cstring>
 using namespace std;
 /**
 Clear all the values to zero.
 */
 CropInfo::CropInfo(){
     cropCode = 0;
+    name = new char [MAX_NAME_LEN];
+    yieldsByYear = new double [NUM_YEARS];
     for (int index = 0; index < MAX_NAME_LEN; index++) {
         name[index] = '\0';
     }
     for (int index = 0; index < NUM_YEARS; index++) {
         yieldsByYear[index] = 0;
     }
+}
+
+CropInfo::~CropInfo(){
+  delete [] name;
+  delete [] yieldsByYear;
+}
+
+void CropInfo::operator=(const CropInfo &other) {
+     strcpy(name, other.name);
 }
 /**
 Loads the information from the file specified
